b770f9a19ee4187cd65a74c6f6af2c27e6d6c8a0
Darko Vrbaski <vrbaski.ra200.2018@uns.ac.rs>
2022-06-29 04:08:07 +0200

    feat: publish create, update, delete job offers on Dislinkt api

--- Agents/Agents/Service/JobOfferService.cs
+++ Agents/Agents/Service/JobOfferService.cs
@@ -2,12 +2,14 @@
 using Agents.DTO;
 using Agents.Model;
 using Agents.Repository;
+using Agents.Utils;
 using AutoMapper;
 
 namespace Agents.Service
 {
     public class JobOfferService : IJobOfferService
     {
+        private const string DislinktApiUrl = "http://localhost:8000/jobOffer";
         private readonly IJobOfferRepository _jobOfferRepository;
         private readonly ISkillRepository _skillRepository;
         private readonly IMapper _mapper;
@@ -37,18 +39,37 @@ namespace Agents.Service
         public JobOffer PostNewJobOffer(JobOfferDTO jobOfferDTO)
         {
             JobOffer jobOffer = _mapper.Map<JobOffer>(jobOfferDTO);
+            PublishNewJobOffer(jobOffer);
             return _jobOfferRepository.Insert(jobOffer);
+        }
+
+        private void PublishNewJobOffer(JobOffer jobOffer)
+        {
+            _ = APICall.PostAsync(DislinktApiUrl, "", jobOffer);
         }
 
         public JobOffer UpdateJobOffer(JobOfferDTO jobOfferDTO)
         {
             JobOffer jobOffer = _mapper.Map<JobOffer>(jobOfferDTO);
+            PublishJobOfferUpdate(jobOffer);
             return _jobOfferRepository.Update(jobOffer);
+        }
+
+        private void PublishJobOfferUpdate(JobOffer jobOffer)
+        {
+            _ = APICall.PutAsync(DislinktApiUrl, "", jobOffer);
         }
 
         public void DeleteJobOffer(long id)
         {
+            JobOffer jobOffer = _jobOfferRepository.Get(id);
+            PublishJobOfferDelete(jobOffer);
             _jobOfferRepository.Delete(id);
+        }
+
+        private void PublishJobOfferDelete(JobOffer jobOffer)
+        {
+            _ = APICall.DeleteAsync<JobOffer>(DislinktApiUrl, jobOffer.Id.ToString());
         }
     }
 }
